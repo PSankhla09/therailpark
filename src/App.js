2747150e424d9812653f48db0ee27ef66e008e10
PSankhla09 <f20240451@pilani.bits-pilani.ac.in>
2025-03-24 00:37:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./content2/footer";
 import Home from "./pages/home";
@@ -6,15 +6,13 @@ import Visit from "./pages/visit";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/therailpark" element={<Home />} />
-          <Route path="/visit" element={<Visit />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/therailpark" element={<Home />} />
+        <Route path="/visit" element={<Visit />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
